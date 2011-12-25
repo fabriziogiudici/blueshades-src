1ad615d291128f347c565bcfc5f69b783da67556
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-12-25 12:47:05 +0100

    Added MockSpotReadActor.

---
+++ modules/Argyll/src/test/java/it/tidalwave/argyll/impl/MockSpotReadActor.java
@@ -0,0 +1,73 @@
+/***********************************************************************************************************************
+ *
+ * blueArgyle - a Java UI for Argyll
+ * Copyright (C) 2011-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://blueargyle.java.net
+ * SCM: https://bitbucket.org/tidalwave/blueargyle-src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.argyll.impl;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
+import java.io.IOException;
+import it.tidalwave.actor.annotation.Actor;
+import it.tidalwave.actor.annotation.MessageListener;
+import it.tidalwave.colorimetry.ColorPoint;
+import it.tidalwave.colorimetry.ColorPoints;
+import it.tidalwave.colorimetry.ColorTemperature;
+import it.tidalwave.colorimetry.MeasureWithPrecision;
+import it.tidalwave.argyll.MeasurementMessage;
+import it.tidalwave.argyll.MeasurementRequest;
+import lombok.extern.slf4j.Slf4j;
+
+/***********************************************************************************************************************
+ * 
+ * @stereotype Actor
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@Actor(threadSafe=false) @NotThreadSafe @Slf4j
+public class MockSpotReadActor 
+  {
+    /*******************************************************************************************************************
+     * 
+     * Answers to the request for a measurement.
+     * 
+     ******************************************************************************************************************/
+    @MessageListener
+    public void spotRead (final @Nonnull MeasurementRequest message)
+      throws InterruptedException
+      {
+        log.info("spotRead({})", message);
+
+        Thread.sleep(500);
+        
+        final ColorPoint xyz = new ColorPoint(0.1f, 0.2f, 0.3f, ColorPoint.ColorSpace.XYZ);
+        final ColorPoint lab = new ColorPoint(100f, 101f, 102f, ColorPoint.ColorSpace.Lab);
+        
+        final ColorPoints colorPoints = new ColorPoints(lab, xyz);
+        
+        final MeasureWithPrecision<ColorTemperature> ccTemp        = new MeasureWithPrecision<ColorTemperature>(new ColorTemperature(2390, ColorTemperature.Scale.K), 0.202257);
+        final MeasureWithPrecision<ColorTemperature> planckianTemp = new MeasureWithPrecision<ColorTemperature>(new ColorTemperature(2389, ColorTemperature.Scale.K), 0.110858);
+        final MeasureWithPrecision<ColorTemperature> daylightTemp  = new MeasureWithPrecision<ColorTemperature>(new ColorTemperature(2244, ColorTemperature.Scale.K), 13.893159);
+        
+        new MeasurementMessage(colorPoints, ccTemp, planckianTemp, daylightTemp).send();
+      }
+  }
