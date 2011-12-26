286619598989e7fc1620dd027afe1b5377463c9b
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-12-26 10:26:37 +0100

    ...

--- modules/Uniformity/src/main/java/it/tidalwave/uniformity/ModuleActorGroupActivator.java
+++ modules/Uniformity/src/main/java/it/tidalwave/uniformity/ModuleActorGroupActivator.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  **********************************************************************************************************************/
 @Slf4j
-public class ModuleActorGroupActivator extends ModuleInstall
+public class ModuleActorGroupActivator extends ModuleInstall // FIXME: move to Actors
   {
     private final ActorGroupActivator actorActivator;
 
