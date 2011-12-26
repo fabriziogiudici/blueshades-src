264a6bb19742a53f563f1095df67b03e44044fca
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-12-26 17:11:21 +0100

    Clean up.

--- modules/Uniformity/src/test/java/it/tidalwave/uniformity/measurement/ui/impl/UniformityCheckMeasurementControllerActorTest.java
+++ modules/Uniformity/src/test/java/it/tidalwave/uniformity/measurement/ui/impl/UniformityCheckMeasurementControllerActorTest.java
@@ -34,7 +34,6 @@ import it.tidalwave.uniformity.measurement.ui.UniformityCheckMeasurementPresenta
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import static org.mockito.Mockito.*;
--- modules/UniformityNetBeans/src/test/java/it/tidalwave/uniformity/measurement/ui/impl/UniformityCheckMeasurementControllerActorIntegrationTest.java
+++ modules/UniformityNetBeans/src/test/java/it/tidalwave/uniformity/measurement/ui/impl/UniformityCheckMeasurementControllerActorIntegrationTest.java
@@ -35,7 +35,6 @@ import it.tidalwave.uniformity.measurement.ui.impl.netbeans.NetBeansUniformityCh
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import static org.mockito.Mockito.*;
