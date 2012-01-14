684d89efab3d2a8689273d03748a63413d134d37
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2012-01-14 22:50:24 +0100

    Added skeleton for the Profile Evaluation.

---
+++ modules/ProfileEvaluationUI/src/main/java/it/tidalwave/blueargyle/profileevaluation/ui/ProfileEvaluationActorActivator.java
@@ -0,0 +1,17 @@
+/***********************************************************************************************************************
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ **********************************************************************************************************************/
+package it.tidalwave.blueargyle.profileevaluation.ui;
+
+import it.tidalwave.actor.spi.ActorGroupActivator;
+
+/***********************************************************************************************************************
+ *
+ * @author  fritz
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class ProfileEvaluationActorActivator extends ActorGroupActivator
+  {
+  }
